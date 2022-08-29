d3a000242f5559a8617b6da4881455943a015528
Paprikar <40892869+Paprikar@users.noreply.github.com>
2022-08-29 18:14:53 +0300

    Initial commit

---
+++ src/main/java/dev/paprikar/Main.java
@@ -0,0 +1,8 @@
+package dev.paprikar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("VERSION: 1.0");
+    }
+}
---
+++ src/test/java/cicdTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class cicdTest {
+
+    @Test
+    void passingTest() {
+        Assertions.assertTrue(true);
+    }
+}
